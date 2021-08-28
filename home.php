a6a511d96183bcadfc459f89aacf35fdfb4ee40d
Anikkarki <anikkarki5@gmail.com>
2021-08-28 22:16:35 +0545

    Signed-off-by: Anikkarki <anikkarki5@gmail.com>

--- home.php
+++ home.php
@@ -1,8 +1,14 @@
 <html>
+
     <head>
+
         <title>
-        <link rel="stylesheet" href="hstyle.css">
+
         </title>
+        <link rel="stylesheet" href="hstyle.css">
+        <style>
+
+        </style>
     </head>
     <body>
         <div class="topnav">
@@ -11,6 +17,21 @@
             <a href="portfolio.php">Portfolio</a>
         </div>
         <div class="article-show">
+
+        <div class="row">
+  <div class="leftcolumn">
+    <div class="card">
+      <h2>TITLE HEADING</h2>
+      <h5>Title description, Dec 7, 2017</h5>
+      <p>Some text..</p>
+    </div>
+    <div class="card">
+      <h2>TITLE HEADING</h2>
+      <h5>Title description, Sep 2, 2017</h5>
+      <p>Some text..</p>
+    </div>
+  </div>
+  
         </div>
     </body>
 </html>
--- portfolio.php
+++ portfolio.php
@@ -1,16 +1,15 @@
 <html>
     <head>
         <title>
-        <link rel="stylesheet" href="porstyle.css">
         </title>
+        <link rel="stylesheet" href="porstyle.css">
+        
     </head>
     <body>
         <div class="topnav">
             <a href="home.php">Home</a>
             <a href="profile.php">Profile</a>
             <a class="active" href="portfolio.php">Portfolio</a>
-        </div>
-        <div class="article-show">
         </div>
     </body>
 </html>
