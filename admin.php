f76cf6640b3c319de373bb6585d07c94c92ae4ef
Anikkarki <anikkarki5@gmail.com>
2021-09-11 22:14:48 +0545

    core process

--- admin.php
+++ admin.php
@@ -9,21 +9,21 @@
    <div class="blog-write">
    <br>    
    <br>
-   <form>
+   <form method="POST" action="core/admin_blog_process.php" >
    <div class="form-floating mb-3">
-  <input type="text" class="form-control" id="floatingInput" placeholder="title " style="height: 50px; width:800px; margin-left:20px;">
+  <input type="text" class="form-control" id="floatingInput" placeholder="title " style="height: 50px; width:800px; margin-left:20px;" name="title">
   <label for="floatingInput" style="margin-left:25px;">Blog title</label>
 </div>
    <br>
    <div class="form-floating">
-  <textarea class="form-control" placeholder="Leave a comment here" id="floatingTextarea2" style="height: 100px; width:800px; margin-left:20px;"></textarea>
+  <textarea class="form-control" placeholder="Leave a comment here" id="floatingTextarea2" style="height: 100px; width:800px; margin-left:20px;" name="content"></textarea>
   <label for="floatingTextarea2" style="margin-left:30px">Blog Content</label>
 </div>
 <input type="submit" name="submit" style="height: 30px; width:100px; margin-left:20px;  margin-top:20px;">
 </form>
    </div>
    <!--blog section ends here-->
-    
+
    <!--user table section -->
 
 </body>
